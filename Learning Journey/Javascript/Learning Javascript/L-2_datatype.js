214d810e4b5a0a8b991b41b14e55f1ebc5200040
Yeeshu Dwivedi <yeeshuwork@gmail.com>
2025-08-25 20:43:02 +0530

    Lesson 2 added

---
+++ Learning Journey/Javascript/Learning Javascript/L-2_datatype.js
@@ -0,0 +1,34 @@
+"use strict"; // treat all JS Code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not browser
+
+// code readability should be high
+
+let name = "yeeshu" //string data type
+let age = 24 //number
+let isLoggedIn = false //boolean
+let notSet;
+let empty = null;
+
+
+/*  Primitive -
+ number => 2 to power 53
+ bigint
+ string => ""
+ boolean => tue/false
+ null => standalone value or empty value
+ undefined => declared but not assigned
+ symbol = > unique
+ */
+
+/* User Defined - 
+
+Object
+
+*/
+
+console.log(typeof(null))  //object
+
+// The data type of null is Object.
+
+console.log(typeof(undefined))  //undefined
