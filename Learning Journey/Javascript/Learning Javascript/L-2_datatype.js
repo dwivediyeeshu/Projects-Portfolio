f76a281c63ca865947bdb059eed73e9e121a1dcc
Yeeshu Dwivedi <yeeshuwork@gmail.com>
2025-08-25 21:30:21 +0530

    added operations

--- Learning Journey/Javascript/Learning Javascript/L-2_datatype.js
+++ Learning Journey/Javascript/Learning Javascript/L-2_datatype.js
@@ -32,3 +32,35 @@ console.log(typeof(null))  //object
 // The data type of null is Object.
 
 console.log(typeof(undefined))  //undefined
+
+// Conversion & Operation
+
+let score = "33abc"
+
+console.log(typeof(score));
+console.log(typeof score);
+
+let  valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN(Not a number)
+// true => 1; false => 0
+
+let isLogged = 1
+
+let booleanIsLoggedIn = Boolean(isLogged);
+
+console.log(booleanIsLoggedIn);
+
+//"" empty string => false
+// "name" => true
+
+//  --------------------- Operations --------------------------
+
+let value = 3
+let negValue = -value
+console.log(negValue);
