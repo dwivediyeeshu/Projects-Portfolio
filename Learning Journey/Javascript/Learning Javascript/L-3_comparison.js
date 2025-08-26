04ae3ee6848c1d388678153f9d48f60484d5db6d
Yeeshu Dwivedi <yeeshuwork@gmail.com>
2025-08-27 02:30:41 +0530

    Adding more learning lesson

--- Learning Journey/Javascript/Learning Javascript/L-3_comparison.js
+++ Learning Journey/Javascript/Learning Javascript/L-3_comparison.js
@@ -18,3 +18,8 @@ console.log(null >= 0);   // True
           Comparison convert null to a number, treating it as 0.
           Thats why (3) null >= 0 is true and (1) null > 0 is false.
 */
+
+// Strict check - ===
+
+console.log("2" === 2);  // does not convert the string to number & does the strict check
+console.log("2" == 2);  //  convert the string to number & does not strict check
