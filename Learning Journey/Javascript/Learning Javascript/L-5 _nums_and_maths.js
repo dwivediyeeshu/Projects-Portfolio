7ca80cb31c2b10c19728f72f7b1077936b2ff743
Yeeshu Dwivedi <yeeshuwork@gmail.com>
2025-08-28 01:00:45 +0530

    Learned Maths & Numbers

--- Learning Journey/Javascript/Learning Javascript/L-5 _nums_and_maths.js
+++ Learning Journey/Javascript/Learning Javascript/L-5 _nums_and_maths.js
@@ -18,3 +18,30 @@ const hundreds = 1000000;
 
 console.log(hundreds.toLocaleString('en-IN'));                  //make large number easier to read. If we dont give en-IN, the default is the american system
 
+// --------------------- Maths ------------------------------------------------
+
+console.log(Math);
+
+let newNumber = -277
+let decNumber = 4.76
+
+console.log(Math.abs(newNumber));     // 277  - Positive number
+console.log(Math.round(decNumber));   // 5 - Rounding off
+console.log(Math.ceil(decNumber));    // 5 - Upper Value
+console.log(Math.floor(decNumber));   // 4 - Lower Value
+
+console.log(Math.max(4, 6, 7, 9, 21));    // 21 max value
+console.log(Math.min(4, 3, 2, 22, 65, 66));    // 2 min value
+
+
+console.log(Math.random());       // Gives value between  0 & 1
+console.log((Math.random()*10) + 1);           // min value will be 1 
+console.log((Math.random()*10) + 1);  
+
+console.log(Math.floor(Math.random()*10) + 1); 
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);        // formula to get the value in a given range
