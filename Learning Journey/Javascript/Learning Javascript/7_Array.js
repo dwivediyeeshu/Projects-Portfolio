ab3eb4e080d7d2958779500b4ee857bc8cf663a4
Yeeshu Dwivedi <yeeshuwork@gmail.com>
2025-08-29 02:20:07 +0530

    Add Array learning Part 1

--- Learning Journey/Javascript/Learning Javascript/7_Array.js
+++ Learning Journey/Javascript/Learning Javascript/7_Array.js
@@ -34,3 +34,31 @@ console.log(myArr)
 myArr.shift()                   // removes the first element from the array
 
 console.log(myArr)                 
+
+console.log(myArr.includes(21));          // element present in the array or not(true or false)
+console.log(myArr2.indexOf(3));         // location of an element. -1 for elements not existing in the array
+
+const newArr = myArr.join()             // binds & converts the array into string
+
+console.log(newArr);
+
+console.log(typeof newArr);
+
+// Slice & Splice ------------------------------------------------
+console.log("A ", myArr);
+
+const newArrTest1 = myArr.slice(1,3)     // Gives a sub array of elements present at the indices, The range element is not present
+
+console.log(newArrTest1);
+
+console.log("B ", myArr)
+
+const newArrTest2 = myArr.splice(1,3);             // Gives a sub array of elements including the range element.
+
+console.log(newArrTest2);
+
+console.log("C ", myArr);  // [0, 4, 5, 6]
+
+
+// Major difference between slice & splice is that Splice changes the original array (i.e removes the sub array selected in Splice function from the orginal array)
+
