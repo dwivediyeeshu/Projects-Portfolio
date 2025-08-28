4a8eb77432a2bffc99193498562991291c62a702
Yeeshu Dwivedi <yeeshuwork@gmail.com>
2025-08-29 01:25:30 +0530

    add array learning

---
+++ Learning Journey/Javascript/Learning Javascript/7_Array.js
@@ -0,0 +1,29 @@
+//   array
+
+const myArr = [0 , 1 , 2, 3, 4, 5]   // Arrays are resizable in JS & can contain mix of elements
+
+//  JS arrays are not associative arrays.
+
+// JS array-copy operations create shallow copies rather than deep copies. 
+// Shallow copy of an object is a copy who shares same reference as those of the source.
+
+console.log(myArr[0]);
+
+const myHeroes = ["shaktiman", "spiderman", "goku"]
+
+const myArr2 = new Array(1, 2, 3, 4);   // array declaration
+
+console.log(typeof(myArr))                 // object output
+console.log(typeof(myArr2))
+
+// ----- Array Methods ---------
+
+myArr.push(6)
+myArr.push(7)
+
+console.log(myArr);
+
+myArr.pop()        // removes last element in array
+
+console.log(myArr)
+
