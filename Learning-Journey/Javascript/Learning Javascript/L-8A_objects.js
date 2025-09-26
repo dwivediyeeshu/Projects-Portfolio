829d4ff10f106e4c95bed977b26ef1f73e649c15
dwivediyeeshu <yeeshuwork@gmail.com>
2025-09-26 10:11:49 +0530

    Add JS Lessons

---
+++ Learning-Journey/Javascript/Learning Javascript/L-8A_objects.js
@@ -0,0 +1,52 @@
+// singleton
+// Object.create - Constructor method
+
+// object literals
+
+const mySym = Symbol("keys");
+
+const JsUser = {
+  name: "Yeeshu",
+  "full name": "Yeeshu Dwivedi",
+  [mySym]: "keys1",
+  age: 22,
+  location: "Prayag",
+  email: "yeeshu@anime.com",
+  isLoggedIn: false,
+  lastLoginDays: ["Monday", "Sunday"],
+};
+
+let person = { name: "John", age: 25 };
+console.log(person.name); // John
+person.city = "Delhi"; // add property
+delete person.age; // delete property
+console.log(Object.keys(person)); // ["name","city"]
+console.log(Object.values(person)); // ["John","Delhi"]
+
+console.log(JsUser.name);
+console.log(JsUser["name"]); //another way to call a key values from object
+console.log(JsUser["full name"]);
+console.log(JsUser[mySym]);
+
+// to change value -------
+
+JsUser.location = "Kanpur";
+
+//Object.freeze(JsUser);                // used to freeze or not allow any changes to a particular object
+JsUser.location = "USA";
+
+console.log(JsUser);
+
+JsUser.greeting = function () {
+  console.log("Hello JS User");
+};
+
+JsUser.greetingTwo = function () {
+  console.log(`Hello JS User, ${this.name}`);
+};
+
+console.log(JsUser.greeting); // undefined when object is freezed or else [Function (anonymous)] we will get
+
+console.log(JsUser.greeting()); // we will get  error if the object is freezed
+
+console.log(JsUser.greetingTwo());
---
+++ Learning-Journey/Javascript/Learning Javascript/L-8B_Objects.js
@@ -0,0 +1,55 @@
+//const instaUser = new Object(); //singleton object
+const instaUser = {}; // non - singleton object
+
+instaUser.id = "123abc"
+instaUser.name = "John"
+instaUser.isLoggedIn = false
+
+console.log(instaUser);
+
+const regularUser = {
+    email: "john@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Yeeshu",
+            lastname: "Dwivedi"
+        }
+    }
+}
+
+console.log(regularUser.fullname.userfullname.firstname);   // to call a specific value in nested object
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 } // similar problem like array. stores as 2 objects
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+// console.log(instaUser);
+
+// console.log(Object.keys(instaUser));
+// console.log(Object.values(instaUser));
+// console.log(Object.entries(instaUser));
+
+// console.log(instaUser.hasOwnProperty('isLoggedIn'));
