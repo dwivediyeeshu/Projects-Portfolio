f441fd4771d4099020fc783cd768133ffdfc148e
dwivediyeeshu <yeeshuwork@gmail.com>
2025-10-02 21:51:14 +0530

    More lessons

---
+++ Learning-Journey/Javascript/Learning Javascript/iterations/do.js
@@ -0,0 +1,30 @@
+// while Loop
+
+let index = 10
+while (index>=4) {
+
+    console.log(`Value of index is ${index}`);
+    index--;
+    
+}
+
+let myArray = ['Flash', 'Batman', 'Superman']
+
+let arr = 0
+while(arr< myArray.length)
+{
+    console.log(`Value is ${myArray[arr]}`);
+    arr++;
+    
+}
+
+
+// do while loop
+let score = 11
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+    
+
+} while(score <= 10)
--- Learning-Journey/Javascript/Learning Javascript/iterations/for-loop.js
+++ Learning-Journey/Javascript/Learning Javascript/iterations/for-loop.js
@@ -1,20 +1,65 @@
 // for Loop
 
 for (let index = 0; index < 10; index++) {
-    const element = index;
-    if(element == 5){
-        console.log("5 is Best Number");
-    }
-    console.log(element);
-       
+  const element = index;
+  if (element == 5) {
+    console.log("5 is Best Number");
+  }
+  console.log(element);
 }
 
-for (let i = 0; i <= 10; i++) {
+// for (let i = 0; i <= 10; i++) {
+//   console.log(`Outer loop Value : ${i}`);
+
+//   for (let j = 0; j <= 10; j++) {
+//     console.log(`Inner loop value ${j} and inner loop ${i}`);
+//   }
+// }
+
+/*for (let i = 0; i <= 10; i++) {
     console.log(`Outer loop Value : ${i}`);
     
    for (let j = 0; j <= 10; j++) {
     console.log(`Inner loop value ${j} and inner loop ${i}`);
+    console.log(i  + '*' + j + '=' + i*j );
     
    }
     
 }
+   */
+
+let myArray = ["Flash", "batman", "superman"]
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+        
+}
+
+// break and continue statement -------------------------------
+
+for (let i = 1; i <=20 ; i++) {
+    if (i == 5) {
+        console.log(`Detected 5`);
+        break;
+        
+    }
+    console.log(`Value of i is ${i}`);
+    
+}
+
+for (let i = 1; i <=20 ; i++) {
+    if (i == 5) {
+        console.log(`Detected 5`);
+        continue;
+        
+    }
+    console.log(`Value of i is ${i}`);
+    
+}
+
+
+
+
+
+
+
