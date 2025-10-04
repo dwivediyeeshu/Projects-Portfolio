bcfd6632c83fa764bf99e1b23e2e39199eabda40
dwivediyeeshu <yeeshuwork@gmail.com>
2025-10-05 00:53:17 +0530

    Update Learning Progress

---
+++ Learning-Journey/C# & .Net/String-part2/String-part2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace String_part2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "C# is awesome!";
+            //char[] 
+
+            Console.WriteLine(message);
+            Console.WriteLine(message[0]); //C
+            Console.WriteLine(message[1]); // #
+            Console.WriteLine(message[2]); // (space)
+            //Console.WriteLine(message[15]); // System.IndexOutOfRangeException: 'Index was outside the bounds of the array.'
+            Console.WriteLine(message.Length);
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i] + " ");
+                Thread.Sleep(250);     //1000ms = 1sec
+
+            }
+
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}, welcome to C# programming!");
+
+            if(name != "")
+            {
+                Console.WriteLine("0");
+            }
+
+            if(!string.IsNullOrEmpty(name))
+            { 
+                Console.WriteLine("1");    ////null "
+            }
+
+            if(!name.Equals(""))    
+            {
+                Console.WriteLine("2");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Learning-Journey/C# & .Net/string/string/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @string
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path ="C:\\Temp\\MyFile.txt";       // Use double backslashes to escape backslashes in string literals
+            // \t is interpreted as a tab character
+            // \n is interpreted as a newline character
+
+            string speech = "He said \"something\"";     // Use backslash to escape double quotes in string literals
+            string name = "Hello 'World'";      // Single quotes do not need to be escaped in double-quoted string literals
+
+            string verbatimPath = @"C:\Temp\MyFile.txt"; 
+            // Use @ to create a verbatim string literal where backslashes are treated as literal characters
+
+            Console.WriteLine(path);
+            Console.WriteLine(speech);
+            Console.WriteLine(verbatimPath);
+            Console.WriteLine("Your name is " + name);
+
+            // $ is used for string interpolation
+            // $  +
+            //$"Your name is {name}"
+            // "Your name is " + name
+
+            string interpolated = $"Your name is {name}";
+            Console.WriteLine(interpolated);
+            string multiLine = @"This is a
+multi-line
+string literal.";
+
+            Console.WriteLine(multiLine);
+
+            string multiLineInterpolated = $@"This is a
+multi-line
+string literal. Your name is {name}";
+            Console.WriteLine(multiLineInterpolated);
+
+            string empty = string.Empty; // Preferred way to represent an empty string
+            string alsoEmpty = ""; // Valid but less preferred way to represent an empty string
+            string nullString = null; // Represents a null string reference
+
+            char firstChar = name[0]; // Accessing the first character of the string
+            Console.WriteLine($"The first character of your name is '{firstChar}'");
+            Console.WriteLine(empty == alsoEmpty); // True
+            Console.WriteLine(empty == nullString); // False
+
+            Console.Write("Enter your name: ");
+            string newName = Console.ReadLine();
+
+            if (newName == String.Empty)
+            {
+                Console.WriteLine("Please Enter a valid name");
+            }
+            else
+            {
+                Console.WriteLine($"Hello {newName}");
+            }
+
+            // Compare strings
+
+            string a = "Hello";
+            string b = "hello";
+            string c = "Hello";
+
+            // "Hello" != "hello"   Case Sensitive
+
+            if(a == b)
+            {
+                Console.WriteLine("a equals b");
+            }
+            else if(a.Equals(c)){
+                Console.WriteLine("a equals c");
+            }
+            else
+            {
+                Console.WriteLine("a does not equal b");
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
