0f7af1ae9b3b1b355eba8c139c9c09287ec2b6ab
dwivediyeeshu <yeeshuwork@gmail.com>
2025-10-05 01:09:50 +0530

    Update Learning Progress for Csharp

--- Learning-Journey/C# & .Net/Exercises/Exercise5/Exercise5/Program.cs
+++ Learning-Journey/C# & .Net/Exercises/Exercise5/Exercise5/Program.cs
@@ -8,8 +8,31 @@ namespace Exercise5
 {
     internal class Program
     {
+
+        /*
+         * Ask user to input message
+         * Print the message in order
+         * Print the message in reverse order
+         */
+
         static void Main(string[] args)
         {
+            Console.Write("Please enter a message:");
+            string message = Console.ReadLine();
+
+            // Print the message in order
+            Console.WriteLine("Message in order:");
+            Console.WriteLine(message);
+
+            // Print the message in reverse order
+            Console.WriteLine("Message in reverse order:");
+
+            for(int i = message.Length - 1; i>=0; i--)
+            {
+                Console.Write(message[i]);
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Learning-Journey/C# & .Net/Exercises/Exercise6/Exercise6/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise6
+{
+    internal class Program
+    {
+        /*
+         *Ask user to enter password, and store
+         *Ask user to re-enter password, and store
+         * Check if they are both contain something
+         * if so check if they are the same
+         * if they are, print "Password set"
+         * if they are not, print "Passwords do not match"
+         * if they are empty, print "Password cannot be empty"
+         */
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Please enter a password:");
+            string password = Console.ReadLine();
+
+            Console.Write("Please re-enter your password:");
+            string password2 = Console.ReadLine();
+
+            if(!string.IsNullOrEmpty(password))
+            {
+                if(!string.IsNullOrEmpty(password2))
+                {
+                    // Both passwords contain something
+                }
+                else
+                {
+                    Console.WriteLine("Please re-enter your password");
+                    return;
+                }
+                if (password.Equals(password2))
+                {
+                    Console.WriteLine("Password set");
+                }
+                else
+                {
+                    Console.WriteLine("Passwords do not match");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("Please enter a password");
+            }
+
+                Console.ReadLine();
+
+        }
+    }
+}
