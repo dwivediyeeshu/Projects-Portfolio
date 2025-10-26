7b6527edecb9c4fcf492ac9e4ed01d8f0b1cd415
dwivediyeeshu <yeeshuwork@gmail.com>
2025-10-27 02:29:15 +0530

    Complete Promise Lesson

--- Learning-Journey/Javascript/Learning Javascript/AdvanceJS_One/promises.js
+++ Learning-Journey/Javascript/Learning Javascript/AdvanceJS_One/promises.js
@@ -78,3 +78,27 @@ async function consumePromiseFive(){
 }
 
 consumePromiseFive()
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//         const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+// }
+
+//getAllUsers()
+
+fetch('https://api.github.com/users/dwivediyeeshu')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
+
+// promise.all
