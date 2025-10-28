a0fc193504a5cd32271905659879ed8ceb709410
dwivediyeeshu <yeeshuwork@gmail.com>
2025-10-28 09:05:03 +0530

    Asynchronous Code

---
+++ Learning-Journey/Javascript/Learning Javascript/AdvanceJS_One/asynchronous.js
@@ -0,0 +1,42 @@
+// asynchronous means that some operations can happen in the background, without blocking the main thread (the main flow of code).
+
+// JavaScript is single-threaded, meaning it can execute one task at a time. But with asynchronous behavior, it can start a task (like fetching data from an API), continue executing other code, and come back to the first task once it’s done — without freezing the app.
+
+// Synchronous -
+console.log("Start");
+
+function task() {
+  console.log("Task complete");
+}
+
+task();
+console.log("End");
+
+// Output 
+/*
+Start
+Task complete
+End
+
+Everything runs in order, line by line.
+*/
+
+// Asynchronus
+
+console.log("Start");
+
+setTimeout(() => {
+  console.log("Async Task complete");
+}, 2000);
+
+console.log("End");
+
+// Output 
+/*
+Start
+End
+Async Task complete.
+
+Here, setTimeout() is asynchronous — it schedules a task for later.
+JavaScript doesn’t wait for 2 seconds — it moves on and runs the rest of the code.
+*/
