b6d3d889dfa214b5a61c92ab36dc566d478bef5f
dwivediyeeshu <yeeshuwork@gmail.com>
2025-11-10 06:23:14 +0530

    Added Async and Sync difference notes

---
+++ Learning-Journey/Backend Development/NodeJs/01-Modules/script.js
@@ -0,0 +1,21 @@
+const { error } = require("node:console");
+const fs = require("node:fs");
+
+// Task - Read the contents of notes.txt
+
+console.log("Start of the Script");
+
+// Sync => Blocking Operations
+// const content = fs.readFileSync("notes.txt", "utf-8");
+// console.log("Contents", content);
+
+// [Async] => Non Blocking
+fs.readFile("notes.txt", "utf-8", (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Content got", data);
+  }
+});
+
+console.log("End of Script");
