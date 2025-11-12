c37211e1da8c3dd51ca090a7e53ed46dc53241a5
dwivediyeeshu <yeeshuwork@gmail.com>
2025-11-12 15:32:34 +0530

    MyEventClass added to the notes

---
+++ Learning-Journey/Backend Development/NodeJs/02-Events/myEventClass.js
@@ -0,0 +1,18 @@
+const EventEmitter = require('events');
+
+class Chat extends EventEmitter{
+    sendMessage(msg){
+        console.log(`Message sent: ${msg}`);
+        this.emit('messageReceived', msg)
+        
+    }
+}
+
+const chat = new Chat()
+chat.on('messageReceived', (msg) => {
+    console.log(`New Message: ${msg}`);
+    
+});
+
+// trigger event
+chat.sendMessage('Hello John Cena')
